dbe3f2b85e445c0e9bf5ad28c402959523b266f8
Jordan Brockopp <Jordan.Brockopp@target.com>
2019-11-14 12:52:36 -0600

    add tests to verify anchor functionality

--- yaml/build_test.go
+++ yaml/build_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/go-vela/types/raw"
 
-	yaml "gopkg.in/yaml.v2"
+	"github.com/buildkite/yaml"
 )
 
 func TestYaml_Build_UnmarshalYAML(t *testing.T) {
@@ -206,6 +206,250 @@ func TestYaml_Build_UnmarshalYAML(t *testing.T) {
 
 	// run test
 	b, err := ioutil.ReadFile("testdata/build.yml")
+	if err != nil {
+		t.Errorf("Reading file for UnmarshalYAML returned err: %v", err)
+	}
+
+	err = yaml.Unmarshal(b, got)
+
+	if err != nil {
+		t.Errorf("UnmarshalYAML returned err: %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("UnmarshalYAML is %v, want %v", got, want)
+	}
+}
+
+func TestYaml_Build_UnmarshalYAML_AnchorStage(t *testing.T) {
+	// setup types
+	want := &Build{
+		Version: "1",
+		Metadata: Metadata{
+			Template: false,
+		},
+		Stages: StageSlice{
+			&Stage{
+				Name:  "dependencies",
+				Needs: []string{"clone"},
+				Steps: StepSlice{
+					&Step{
+						Commands: raw.StringSlice{"./gradlew downloadDependencies"},
+						Environment: raw.StringSliceMap{
+							"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+							"GRADLE_USER_HOME": ".gradle",
+						},
+						Image: "openjdk:latest",
+						Name:  "install",
+						Pull:  true,
+						Ruleset: Ruleset{
+							If:       Rules{Event: []string{"push", "pull_request"}},
+							Operator: "and",
+						},
+						Volumes: VolumeSlice{
+							&Volume{
+								Source:      "/foo",
+								Destination: "/bar",
+								AccessMode:  "ro",
+							},
+						},
+						Ulimits: UlimitSlice{
+							&Ulimit{
+								Name: "foo",
+								Soft: 1024,
+								Hard: 2048,
+							},
+						},
+					},
+				},
+			},
+			&Stage{
+				Name:  "test",
+				Needs: []string{"dependencies"},
+				Steps: StepSlice{
+					&Step{
+						Commands: raw.StringSlice{"./gradlew check"},
+						Environment: raw.StringSliceMap{
+							"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+							"GRADLE_USER_HOME": ".gradle",
+						},
+						Name:  "test",
+						Image: "openjdk:latest",
+						Pull:  true,
+						Ruleset: Ruleset{
+							If:       Rules{Event: []string{"push", "pull_request"}},
+							Operator: "and",
+						},
+						Volumes: VolumeSlice{
+							&Volume{
+								Source:      "/foo",
+								Destination: "/bar",
+								AccessMode:  "ro",
+							},
+						},
+						Ulimits: UlimitSlice{
+							&Ulimit{
+								Name: "foo",
+								Soft: 1024,
+								Hard: 2048,
+							},
+						},
+					},
+				},
+			},
+			&Stage{
+				Name:  "build",
+				Needs: []string{"dependencies"},
+				Steps: StepSlice{
+					&Step{
+						Commands: raw.StringSlice{"./gradlew build"},
+						Environment: raw.StringSliceMap{
+							"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+							"GRADLE_USER_HOME": ".gradle",
+						},
+						Name:  "build",
+						Image: "openjdk:latest",
+						Pull:  true,
+						Ruleset: Ruleset{
+							If:       Rules{Event: []string{"push", "pull_request"}},
+							Operator: "and",
+						},
+						Volumes: VolumeSlice{
+							&Volume{
+								Source:      "/foo",
+								Destination: "/bar",
+								AccessMode:  "ro",
+							},
+						},
+						Ulimits: UlimitSlice{
+							&Ulimit{
+								Name: "foo",
+								Soft: 1024,
+								Hard: 2048,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	got := new(Build)
+
+	// run test
+	b, err := ioutil.ReadFile("testdata/build_anchor_stage.yml")
+	if err != nil {
+		t.Errorf("Reading file for UnmarshalYAML returned err: %v", err)
+	}
+
+	err = yaml.Unmarshal(b, got)
+
+	if err != nil {
+		t.Errorf("UnmarshalYAML returned err: %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("UnmarshalYAML is %v, want %v", got, want)
+	}
+}
+
+func TestYaml_Build_UnmarshalYAML_AnchorSteps(t *testing.T) {
+	// setup types
+	want := &Build{
+		Version: "1",
+		Metadata: Metadata{
+			Template: false,
+		},
+		Steps: StepSlice{
+			&Step{
+				Commands: raw.StringSlice{"./gradlew downloadDependencies"},
+				Environment: raw.StringSliceMap{
+					"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+					"GRADLE_USER_HOME": ".gradle",
+				},
+				Image: "openjdk:latest",
+				Name:  "install",
+				Pull:  true,
+				Ruleset: Ruleset{
+					If:       Rules{Event: []string{"push", "pull_request"}},
+					Operator: "and",
+				},
+				Volumes: VolumeSlice{
+					&Volume{
+						Source:      "/foo",
+						Destination: "/bar",
+						AccessMode:  "ro",
+					},
+				},
+				Ulimits: UlimitSlice{
+					&Ulimit{
+						Name: "foo",
+						Soft: 1024,
+						Hard: 2048,
+					},
+				},
+			},
+			&Step{
+				Commands: raw.StringSlice{"./gradlew check"},
+				Environment: raw.StringSliceMap{
+					"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+					"GRADLE_USER_HOME": ".gradle",
+				},
+				Name:  "test",
+				Image: "openjdk:latest",
+				Pull:  true,
+				Ruleset: Ruleset{
+					If:       Rules{Event: []string{"push", "pull_request"}},
+					Operator: "and",
+				},
+				Volumes: VolumeSlice{
+					&Volume{
+						Source:      "/foo",
+						Destination: "/bar",
+						AccessMode:  "ro",
+					},
+				},
+				Ulimits: UlimitSlice{
+					&Ulimit{
+						Name: "foo",
+						Soft: 1024,
+						Hard: 2048,
+					},
+				},
+			},
+			&Step{
+				Commands: raw.StringSlice{"./gradlew build"},
+				Environment: raw.StringSliceMap{
+					"GRADLE_OPTS":      "-Dorg.gradle.daemon=false -Dorg.gradle.workers.max=1 -Dorg.gradle.parallel=false",
+					"GRADLE_USER_HOME": ".gradle",
+				},
+				Name:  "build",
+				Image: "openjdk:latest",
+				Pull:  true,
+				Ruleset: Ruleset{
+					If:       Rules{Event: []string{"push", "pull_request"}},
+					Operator: "and",
+				},
+				Volumes: VolumeSlice{
+					&Volume{
+						Source:      "/foo",
+						Destination: "/bar",
+						AccessMode:  "ro",
+					},
+				},
+				Ulimits: UlimitSlice{
+					&Ulimit{
+						Name: "foo",
+						Soft: 1024,
+						Hard: 2048,
+					},
+				},
+			},
+		},
+	}
+	got := new(Build)
+
+	// run test
+	b, err := ioutil.ReadFile("testdata/build_anchor_step.yml")
 	if err != nil {
 		t.Errorf("Reading file for UnmarshalYAML returned err: %v", err)
 	}

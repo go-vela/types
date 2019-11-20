3124b5fd0366dcdf71355a5d03d84b2145e873c7
David Vader <48764154+davidvader@users.noreply.github.com>
2019-11-20 09:58:09 -0600

    adding build timeout constants (#18)

---
+++ constants/limit.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// queue types
+const (
+	// BuildTimeoutMin defines the minimum value in minutes for repo build timeout.
+	BuildTimeoutMin = 30
+	// BuildTimeoutMax defines the maximum value in minutes for repo build timeout.
+	BuildTimeoutMax = 90
+)

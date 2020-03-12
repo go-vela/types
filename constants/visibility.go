e53c59b86d0371429e55b83b67022f9ba554fb42
Jordan Brockopp <jordan.brockopp@target.com>
2020-03-12 10:11:50 -0500

    feat: add repo visibility types (#40)
    * fix: typo in spacing for license
    
    * docs: move license badge to top of readme
    
    * feat(constants): add repo visibility types
    
    * feat(database): validate repo visibility field

---
+++ constants/visibility.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// repo visibility types
+const (
+	// VisibilityPublic defines the visibility type for allowing any
+	// users in Vela to access their repo regardless of the access
+	// defined in the source control system.
+	VisibilityPublic = "public"
+
+	// VisibilityPrivate defines the visibility type for only allowing
+	// users in Vela with pre-defined access in the source control
+	// system to access their repo.
+	VisibilityPrivate = "private"
+)

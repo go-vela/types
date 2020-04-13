bdf92bf767f986244e2e23bfc46f6487081787fd
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-04-13 12:13:22 -0500

    feat(constants): add pipeline types

---
+++ constants/pipeline.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// pipeline types
+const (
+	// PipelineStages defines the type for a pipeline with stages.
+	PipelineStage = "stages"
+
+	// PipelineStep defines the type for a pipeline with steps.
+	PipelineStep = "steps"
+
+	// PipelineTemplate defines the type for a pipeline as a template.
+	PipelineTemplate = "template"
+)

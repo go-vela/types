70deef9c74c624f75acd628a402d1cc6350ccfa8
Jordan <Jordan.Brockopp@target.com>
2020-10-15 14:47:57 -0500

    feat: add metadata struct

--- version/version.go
+++ version/version.go
@@ -39,23 +39,15 @@ type Version struct {
 	// Full represents a fully functional semantic version for the application.
 	Full string `json:"full,omitempty"`
 	// Metadata represents extra information surrounding the application version.
-	Metadata struct {
-		// Architecture represents the architecture information for the application.
-		Architecture string `json:"architecture,omitempty"`
-		// BuildDate represents the build date information for the application.
-		BuildDate string `json:"build_date,omitempty"`
-		// Compiler represents the compiler information for the application.
-		Compiler string `json:"compiler,omitempty"`
-		// GitCommit represents the git commit information for the application.
-		GitCommit string `json:"git_commit,omitempty"`
-		// GoVersion represents the golang version information for the application.
-		GoVersion string `json:"go_version,omitempty"`
-		// OperatingSystem represents the operating system information for the application.
-		OperatingSystem string `json:"operating_system,omitempty"`
-	} `json:"metadata,omitempty"`
+	Metadata Metadata
 }
 
-// Semantic implements a semantic string of the Stringer interface for the Version type.
+// Meta implements a formatted string containing only metadata for the Version type.
+func (v *Version) Meta() string {
+	return v.Metadata.String()
+}
+
+// Semantic implements a formatted string containing a formal semantic version for the Version type.
 func (v *Version) Semantic() string {
 	return v.Full
 }

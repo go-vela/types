80f6993a8da7ebdaa5bc13f697b31a8a8d101bd1
kneal <neal.coleman@target.com>
2020-12-10 12:02:55 -0600

    refactor: move linting from compiler to types

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/go-vela/types
 go 1.15
 
 require (
+	github.com/docker/distribution v2.7.1+incompatible
 	github.com/ghodss/yaml v1.0.0
 	github.com/goccy/go-yaml v1.8.4
 	github.com/lib/pq v1.8.0
+	github.com/opencontainers/go-digest v1.0.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- yaml/stage_test.go
+++ yaml/stage_test.go
@@ -281,3 +281,60 @@ func TestYaml_StageSlice_UnmarshalYAML(t *testing.T) {
 		}
 	}
 }
+
+func TestStageSlice_Validate(t *testing.T) {
+	//setup types
+	tests := []struct {
+		name    string
+		file    string
+		wantErr bool
+	}{
+		{
+			name:    "success: minimal stage block",
+			file:    "testdata/stage/validate/minimal.yml",
+			wantErr: false,
+		},
+		{
+			name:    "failure: missing commands, environment, parameters, secrets or template yaml tag",
+			file:    "testdata/stage/validate/missing.yml",
+			wantErr: true,
+		},
+		{
+			name:    "failure: missing name yaml tag",
+			file:    "testdata/stage/validate/missing_name.yml",
+			wantErr: true,
+		},
+		{
+			name:    "failure: missing image yaml tag",
+			file:    "testdata/stage/validate/missing_image.yml",
+			wantErr: true,
+		},
+		{
+			name:    "failure: bad image tag data",
+			file:    "testdata/stage/validate/bad_image.yml",
+			wantErr: true,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		b := new(Build)
+
+		pipeline, err := ioutil.ReadFile(test.file)
+		if err != nil {
+			t.Errorf("Reading file for Validate returned err: %v", err)
+		}
+
+		err = yaml.Unmarshal(pipeline, b)
+
+		if err != nil {
+			t.Errorf("Validate returned err: %v", err)
+		}
+
+		t.Run(test.name, func(t *testing.T) {
+			if err := b.Stages.Validate(pipeline); (err != nil) != test.wantErr {
+				t.Errorf("Validate is %v, want %v", err, test.wantErr)
+			}
+		})
+	}
+}

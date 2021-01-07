8e6bfe7711fb0d9e10cbd577e9b8266410283ab0
Jordan Sussman <jordansail22@gmail.com>
2021-01-07 15:34:20 -0600

    feat(yaml): allow pipeline to specify metadata.clone bool (#130)
    * feat(yaml): allow pipeline to specify metadata.clone bool
    
    * Update yaml/metadata.go
    
    Co-authored-by: Jordan Brockopp <jordan.brockopp@target.com>
    
    Co-authored-by: Jordan Brockopp <jordan.brockopp@target.com>

--- yaml/metadata.go
+++ yaml/metadata.go
@@ -10,13 +10,21 @@ import "github.com/go-vela/types/pipeline"
 // the metadata block for a pipeline.
 // nolint:lll // jsonschema will cause long lines
 type Metadata struct {
-	Template bool `yaml:"template,omitempty" json:"template,omitempty" jsonschema:"description=Enables compiling the pipeline as a template.\nReference: https://go-vela.github.io/docs/concepts/pipeline/metadata/"`
+	Template bool  `yaml:"template,omitempty" json:"template,omitempty" jsonschema:"description=Enables compiling the pipeline as a template.\nReference: https://go-vela.github.io/docs/concepts/pipeline/metadata/"`
+	Clone    *bool `yaml:"clone,omitempty" json:"clone,omitempty" jsonschema:"default=true,description=Enables injecting the default clone process.\nReference: https://go-vela.github.io/docs/concepts/pipeline/metadata/"`
 }
 
 // ToPipeline converts the Metadata type
 // to a pipeline Metadata type.
 func (m *Metadata) ToPipeline() *pipeline.Metadata {
+	var clone bool
+	if m.Clone == nil {
+		clone = true
+	} else {
+		clone = *m.Clone
+	}
 	return &pipeline.Metadata{
 		Template: m.Template,
+		Clone:    clone,
 	}
 }

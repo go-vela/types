e83f7d1b1c1584fd49bbde660f60b8e9fd5da5be
David May <david.may@target.com>
2021-01-09 02:22:45 -0600

    fix: remove unneeded code

--- library/login.go
+++ library/login.go
@@ -21,49 +21,7 @@ import "fmt"
 //
 // swagger:model Login
 type Login struct {
-	Username *string `json:"username,omitempty"`
-	Password *string `json:"password,omitempty"`
-	OTP      *string `json:"otp,omitempty"`
-	Token    *string `json:"token,omitempty"`
-}
-
-// GetUsername returns the Username field.
-//
-// When the provided Login type is nil, or the field within
-// the type is nil, it returns the zero value for the field.
-func (l *Login) GetUsername() string {
-	// return zero value if Login type or Username field is nil
-	if l == nil || l.Username == nil {
-		return ""
-	}
-
-	return *l.Username
-}
-
-// GetPassword returns the Password field.
-//
-// When the provided Login type is nil, or the field within
-// the type is nil, it returns the zero value for the field.
-func (l *Login) GetPassword() string {
-	// return zero value if Login type or Password field is nil
-	if l == nil || l.Password == nil {
-		return ""
-	}
-
-	return *l.Password
-}
-
-// GetOTP returns the Username field.
-//
-// When the provided Login type is nil, or the field within
-// the type is nil, it returns the zero value for the field.
-func (l *Login) GetOTP() string {
-	// return zero value if Login type or OTP field is nil
-	if l == nil || l.OTP == nil {
-		return ""
-	}
-
-	return *l.OTP
+	Token *string `json:"token,omitempty"`
 }
 
 // GetToken returns the Token field.
@@ -77,45 +35,6 @@ func (l *Login) GetToken() string {
 	}
 
 	return *l.Token
-}
-
-// SetUsername sets the Username field.
-//
-// When the provided Login type is nil, it
-// will set nothing and immediately return.
-func (l *Login) SetUsername(v string) {
-	// return if Login type is nil
-	if l == nil {
-		return
-	}
-
-	l.Username = &v
-}
-
-// SetPassword sets the Password field.
-//
-// When the provided Login type is nil, it
-// will set nothing and immediately return.
-func (l *Login) SetPassword(v string) {
-	// return if Login type is nil
-	if l == nil {
-		return
-	}
-
-	l.Password = &v
-}
-
-// SetOTP sets the OTP field.
-//
-// When the provided Login type is nil, it
-// will set nothing and immediately return.
-func (l *Login) SetOTP(v string) {
-	// return if Login type is nil
-	if l == nil {
-		return
-	}
-
-	l.OTP = &v
 }
 
 // SetToken sets the Token field.
@@ -134,14 +53,8 @@ func (l *Login) SetToken(v string) {
 // String implements the Stringer interface for the Login type.
 func (l *Login) String() string {
 	return fmt.Sprintf(`{
-  OTP: %s,
-  Password: %s,
   Token: %s,
-  Username: %s,
 }`,
-		l.GetOTP(),
-		l.GetPassword(),
 		l.GetToken(),
-		l.GetUsername(),
 	)
 }

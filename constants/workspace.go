795d689a3f11d97eff2a89e1f1f0d9da6e8b02f1
Jordan <Jordan.Brockopp@target.com>
2021-01-15 07:57:49 -0600

    feat: add workspace constants for services and steps

---
+++ constants/workspace.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// Service and Step workspace paths.
+const (
+	// WorkspaceDefault defines the default workspace path for a service or a step.
+	WorkspaceDefault = "/vela/src"
+
+	// WorkspaceMount defines the mount workspace path for a service or a step.
+	WorkspaceMount = "/vela"
+)

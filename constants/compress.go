9d8e582c183107365bf250c2fded584557745739
Jordan <Jordan.Brockopp@target.com>
2021-02-18 15:20:21 -0600

    feat(constants): add compression level

---
+++ constants/compress.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// compression level for log data stored in the database.
+const CompressionLevel = 3

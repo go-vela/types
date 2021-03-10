2d46c7dbe49635fcb9919c890eb1a387346f79fb
Jordan <Jordan.Brockopp@target.com>
2021-03-10 15:05:32 -0600

    feat(database): add repo Encrypt() and Decrypt()

--- database/repo.go
+++ database/repo.go
@@ -5,8 +5,14 @@
 package database
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"database/sql"
+	"encoding/base64"
 	"errors"
+	"fmt"
+	"io"
 
 	"github.com/go-vela/types/library"
 )
@@ -58,6 +64,108 @@ type Repo struct {
 	AllowDeploy  sql.NullBool   `sql:"allow_deploy"`
 	AllowTag     sql.NullBool   `sql:"allow_tag"`
 	AllowComment sql.NullBool   `sql:"allow_comment"`
+}
+
+// Decrypt will manipulate the existing repo hash by
+// base64 decoding that value. Then, a AES-256 cipher
+// block is created from the encryption key in order to
+// decrypt the base64 decoded secret value.
+func (r *Repo) Decrypt(key string) error {
+	// base64 decode the encrypted repo hash
+	decoded, err := base64.StdEncoding.DecodeString(r.Hash.String)
+	if err != nil {
+		return err
+	}
+
+	// create a new cipher block from the encryption key
+	//
+	// the key should have a length of 64 bits to ensure
+	// we are using the AES-256 standard
+	//
+	// https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return err
+	}
+
+	// creates a new Galois Counter Mode cipher block
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// nonce is an arbitrary number used to to ensure that
+	// old communications cannot be reused in replay attacks.
+	//
+	// https://en.wikipedia.org/wiki/Cryptographic_nonce
+	nonceSize := gcm.NonceSize()
+
+	// verify the decoded repo hash is greater than nonce
+	//
+	// if the base64 decoded repo hash is less than the
+	// nonce size, then we can reasonably assume the repo
+	// hasn't been encrypted yet.
+	if len(decoded) < nonceSize {
+		return fmt.Errorf("invalid length for decoded repo hash")
+	}
+
+	// capture nonce and ciphertext from decoded repo hash
+	nonce, ciphertext := decoded[:nonceSize], decoded[nonceSize:]
+
+	// decrypt the decoded repo hash from the ciphertext
+	decrypted, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	// set the decrypted repo hash
+	r.Hash = sql.NullString{String: string(decrypted), Valid: true}
+
+	return nil
+}
+
+// Encrypt will manipulate the existing repo hash by
+// creating a AES-256 cipher block from the encryption
+// key in order to encrypt the repo hash. Then, the
+// repo hash is base64 encoded for transport across
+// network boundaries.
+func (r *Repo) Encrypt(key string) error {
+	// create a new cipher block from the encryption key
+	//
+	// the key should have a length of 64 bits to ensure
+	// we are using the AES-256 standard
+	//
+	// https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return err
+	}
+
+	// creates a new Galois Counter Mode cipher block
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// nonce is an arbitrary number used to to ensure that
+	// old communications cannot be reused in replay attacks.
+	//
+	// https://en.wikipedia.org/wiki/Cryptographic_nonce
+	nonce := make([]byte, gcm.NonceSize())
+
+	// set nonce from a cryptographically secure random number generator
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return err
+	}
+
+	// encrypt the data with the randomly generated nonce
+	encrypted := gcm.Seal(nonce, nonce, []byte(r.Hash.String), nil)
+
+	// base64 encode the encrypted repo hash to make it network safe
+	r.Hash = sql.NullString{String: base64.StdEncoding.EncodeToString(encrypted), Valid: true}
+
+	return nil
 }
 
 // Nullify ensures the valid flag for

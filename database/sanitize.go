64103d0e747d462a3d59923d1dd2c187ae0dd3a6
Jordan <Jordan.Brockopp@target.com>
2021-04-15 11:04:43 -0500

    enhance: handle query escaped html input

--- database/sanitize.go
+++ database/sanitize.go
@@ -6,23 +6,35 @@ package database
 
 import (
 	"html"
+	"net/url"
 	"strings"
 
 	"github.com/microcosm-cc/bluemonday"
 )
 
 // sanitize is a helper function to verify the provided input
-// does not contain HTML content. If the input does contain
-// HTML, then the function will return an error.
+// field does not contain HTML content. If the input field
+// does contain HTML, then the function will sanitize and
+// potentially remove the HTML if deemed malicious.
 func sanitize(field string) string {
 	// create new HTML input microcosm-cc/bluemonday policy
+	//
+	// NOTE: consider using bluemonday.UGCPolicy() if the
+	// strict policy has too much user impact
 	p := bluemonday.StrictPolicy()
 
-	// create an HTML escaped string from the field
-	htmlEscaped := html.EscapeString(field) // &#34;hello&#34;
+	// create a URL query unescaped string from the field
+	queryUnescaped, err := url.QueryUnescape(field)
+	if err != nil {
+		// overwrite URL query unescaped string with field
+		queryUnescaped = field
+	}
 
-	// create a bluemonday escaped string from the field
-	bluemondayEscaped := p.Sanitize(field) // &#34;hello&#34;
+	// create an HTML escaped string from the field
+	htmlEscaped := html.EscapeString(queryUnescaped)
+
+	// create a microcosm-cc/bluemonday escaped string from the field
+	bluemondayEscaped := p.Sanitize(queryUnescaped)
 
 	// check if the field contains html
 	if !strings.EqualFold(htmlEscaped, bluemondayEscaped) {
--- database/sanitize_test.go
+++ database/sanitize_test.go
@@ -15,15 +15,47 @@ func TestDatabase_Sanitize(t *testing.T) {
 		want  string
 	}{
 		{
+			value: `%`,
+			want:  `%`,
+		},
+		{
 			value: `"hello"`,
 			want:  `"hello"`,
+		},
+		{
+			value: `OctoKitty@github.com`,
+			want:  `OctoKitty@github.com`,
+		},
+		{
+			value: `https://github.com/go-vela`,
+			want:  `https://github.com/go-vela`,
+		},
+		{
+			value: `Merge pull request #1 from me/patch-1\n\n<h1>hello</h1> is now <h2>hello</h2>`,
+			want:  `Merge pull request #1 from me/patch-1\n\nhello is now hello`,
+		},
+		{
+			value: `+ added foo %25 + updated bar %22 +`,
+			want:  `+ added foo %25 + updated bar %22 +`,
+		},
+		{
+			value: `Co-authored-by: OctoKitty <OctoKitty@github.com>`,
+			want:  `Co-authored-by: OctoKitty `,
+		},
+		{
+			value: `<a onblur="alert(secret)" href="http://www.google.com">Google</a>`,
+			want:  `Google`,
+		},
+		{
+			value: `<script>alert('XSS')</script>`,
+			want:  ``,
 		},
 		{
 			value: `<SCRIPT/XSS SRC="http://ha.ckers.org/xss.js"></SCRIPT>`,
 			want:  ``,
 		},
 		{
-			value: `<script>alert('XSS')</script>`,
+			value: `%3cDIV%20STYLE%3d%22width%3a%20expression(alert('XSS'))%3b%22%3e`,
 			want:  ``,
 		},
 	}

cdf5a845f58f741c5f5b8cd438e8206893e8662b
kneal <neal.coleman@target.com>
2021-07-08 15:06:05 -0500

    chore: add constansts for pipeline types

---
+++ constants/repo.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package constants
+
+// Repo get pipeline types.
+const (
+	// PipelineTypeYAML defines the pipeline type for allowing users
+	// in Vela to control their pipeline being compiled as yaml.
+	PipelineTypeYAML = "yaml"
+
+	// PipelineTypeGo defines the pipeline type for allowing users
+	// in Vela to control their pipeline being compiled as Go templates.
+	PipelineTypeGo = "yaml"
+
+	// PipelineTypeStarlark defines the pipeline type for allowing users
+	// in Vela to control their pipeline being compiled as Starlark templates.
+	PipelineTypeStarlark = "yaml"
+)

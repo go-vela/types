2d817e4b7c9abdffd597659c581b5947ce8d502f
JordanBrockopp <Jordan_Brockopp@cargill.com>
2022-01-07 23:44:38 -0600

    feat(database): add compress() and decompress()

---
+++ database/compression.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"bytes"
+	"compress/zlib"
+	"io/ioutil"
+)
+
+// compress is a helper function to compress values. First, an
+// empty buffer is created for storing compressed data. Then,
+// a zlib writer, using the DEFLATE algorithm, is created with
+// the provided compression level to output to this buffer.
+// Finally, the provided value is compressed and written to the
+// buffer and the writer is closed which flushes all bytes from
+// the writer to the buffer.
+func compress(level int, value []byte) ([]byte, error) {
+	// create new buffer for storing compressed data
+	b := new(bytes.Buffer)
+
+	// create new zlib writer for outputting data to the buffer in a compressed format
+	w, err := zlib.NewWriterLevel(b, level)
+	if err != nil {
+		return value, err
+	}
+
+	// write data to the buffer in compressed format
+	_, err = w.Write(value)
+	if err != nil {
+		return value, err
+	}
+
+	// close the writer
+	//
+	// compressed bytes are not flushed until the writer is closed or explicitly flushed
+	err = w.Close()
+	if err != nil {
+		return value, err
+	}
+
+	// return compressed bytes from the buffer
+	return b.Bytes(), nil
+}
+
+// decompress is a helper function to decompress values. First, a
+// buffer is created from the provided compressed data. Then, a
+// zlib reader, using the DEFLATE algorithm, is created from the
+// buffer as an input for reading data from the buffer. Finally,
+// the data is decompressed and read from the buffer.
+func decompress(value []byte) ([]byte, error) {
+	// create new buffer from the compressed data
+	b := bytes.NewBuffer(value)
+
+	// create new zlib reader for reading the compressed data from the buffer
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		return value, err
+	}
+
+	// close the reader after the data has been decompressed
+	defer r.Close()
+
+	// capture decompressed data from the compressed data in the buffer
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return value, err
+	}
+
+	return data, nil
+}
---
+++ database/compression_test.go
@@ -0,0 +1,220 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package database
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-vela/types/constants"
+)
+
+func TestDatabase_compress(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		name    string
+		failure bool
+		level   int
+		data    []byte
+		want    []byte
+	}{
+		{
+			name:    "compression level -1",
+			failure: false,
+			level:   constants.CompressionNegOne,
+			data:    []byte("foo"),
+			want:    []byte{120, 156, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 0",
+			failure: false,
+			level:   constants.CompressionZero,
+			data:    []byte("foo"),
+			want:    []byte{120, 1, 0, 3, 0, 252, 255, 102, 111, 111, 1, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 1",
+			failure: false,
+			level:   constants.CompressionOne,
+			data:    []byte("foo"),
+			want:    []byte{120, 1, 0, 3, 0, 252, 255, 102, 111, 111, 1, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 2",
+			failure: false,
+			level:   constants.CompressionTwo,
+			data:    []byte("foo"),
+			want:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 3",
+			failure: false,
+			level:   constants.CompressionThree,
+			data:    []byte("foo"),
+			want:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 4",
+			failure: false,
+			level:   constants.CompressionFour,
+			data:    []byte("foo"),
+			want:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 5",
+			failure: false,
+			level:   constants.CompressionFive,
+			data:    []byte("foo"),
+			want:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 6",
+			failure: false,
+			level:   constants.CompressionSix,
+			data:    []byte("foo"),
+			want:    []byte{120, 156, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 7",
+			failure: false,
+			level:   constants.CompressionSeven,
+			data:    []byte("foo"),
+			want:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 8",
+			failure: false,
+			level:   constants.CompressionEight,
+			data:    []byte("foo"),
+			want:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+		{
+			name:    "compression level 9",
+			failure: false,
+			level:   constants.CompressionNine,
+			data:    []byte("foo"),
+			want:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		got, err := compress(test.level, test.data)
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("compress for %s should have returned err", test.name)
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("compress for %s returned err: %v", test.name, err)
+		}
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("compress for %s is %v, want %v", test.name, string(got), string(test.want))
+		}
+	}
+}
+
+func TestDatabase_decompress(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		name    string
+		failure bool
+		data    []byte
+		want    []byte
+	}{
+		{
+			name:    "compression level -1",
+			failure: false,
+			data:    []byte{120, 156, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 0",
+			failure: false,
+			data:    []byte{120, 1, 0, 3, 0, 252, 255, 102, 111, 111, 1, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 1",
+			failure: false,
+			data:    []byte{120, 1, 0, 3, 0, 252, 255, 102, 111, 111, 1, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 2",
+			failure: false,
+			data:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 3",
+			failure: false,
+			data:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 4",
+			failure: false,
+			data:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 5",
+			failure: false,
+			data:    []byte{120, 94, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 6",
+			failure: false,
+			data:    []byte{120, 156, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 7",
+			failure: false,
+			data:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 8",
+			failure: false,
+			data:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+		{
+			name:    "compression level 9",
+			failure: false,
+			data:    []byte{120, 218, 74, 203, 207, 7, 4, 0, 0, 255, 255, 2, 130, 1, 69},
+			want:    []byte("foo"),
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		got, err := decompress(test.data)
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("decompress for %s should have returned err", test.name)
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("decompressm for %s returned err: %v", test.name, err)
+		}
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("decompress for %s is %v, want %v", test.name, string(got), string(test.want))
+		}
+	}
+}

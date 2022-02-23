15ac9fee0be260eda4c7697e5ca0be21e288e775
JordanBrockopp <Jordan_Brockopp@cargill.com>
2022-02-23 13:55:34 -0600

    feat: support stages with templates

--- yaml/metadata.go
+++ yaml/metadata.go
@@ -8,14 +8,28 @@ import (
 	"github.com/go-vela/types/pipeline"
 )
 
+//version: "1"
+//
+//metadata:
+//render_inline: true # defaults to false
+//
+//templates:
+//- name: go
+//	source: github.com/octocat/hello-world/.vela/build.yml
+//	format: go
+//	type: github
+//	vars:
+//	image: golang:latest
+
 type (
 	// Metadata is the yaml representation of
 	// the metadata block for a pipeline.
 	// nolint:lll // jsonschema will cause long lines
 	Metadata struct {
-		Template    bool     `yaml:"template,omitempty" json:"template,omitempty" jsonschema:"description=Enables compiling the pipeline as a template.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-template-tag"`
-		Clone       *bool    `yaml:"clone,omitempty" json:"clone,omitempty" jsonschema:"default=true,description=Enables injecting the default clone process.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-clone-tag"`
-		Environment []string `yaml:"environment,omitempty" json:"environment,omitempty" jsonschema:"description=Controls which containers processes can have global env injected.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-environment-tag"`
+		Template     bool     `yaml:"template,omitempty" json:"template,omitempty" jsonschema:"description=Enables compiling the pipeline as a template.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-template-tag"`
+		RenderInline bool     `yaml:"render_inline,omitempty" json:"render_inline,omitempty" jsonschema:"description=Enables inline compiling for the pipeline templates.\nReference: TODO"`
+		Clone        *bool    `yaml:"clone,omitempty" json:"clone,omitempty" jsonschema:"default=true,description=Enables injecting the default clone process.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-clone-tag"`
+		Environment  []string `yaml:"environment,omitempty" json:"environment,omitempty" jsonschema:"description=Controls which containers processes can have global env injected.\nReference: https://go-vela.github.io/docs/reference/yaml/metadata/#the-environment-tag"`
 	}
 )
 
@@ -52,9 +66,10 @@ func (m *Metadata) HasEnvironment(container string) bool {
 func (m *Metadata) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	// metadata we try unmarshalling to
 	metadata := new(struct {
-		Template    bool
-		Clone       *bool
-		Environment []string
+		Template     bool
+		RenderInline bool
+		Clone        *bool
+		Environment  []string
 	})
 
 	// attempt to unmarshal as a metadata type
@@ -69,6 +84,7 @@ func (m *Metadata) UnmarshalYAML(unmarshal func(interface{}) error) error {
 
 	// overwrite existing metadata environment details
 	m.Template = metadata.Template
+	m.RenderInline = metadata.RenderInline
 	m.Clone = metadata.Clone
 	m.Environment = metadata.Environment
 

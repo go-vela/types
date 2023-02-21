79b1915f4d578453484e0666f95077fa29cebf6d
ecrupper <easton.crupper12@gmail.com>
2023-02-21 15:25:15 -0600

    remove metadata changes

--- metadata.go
+++ metadata.go
@@ -26,18 +26,6 @@ type (
 		Host   string `json:"host"`
 	}
 
-	// TokenManager is the config data for the v2 token manager.
-	TokenManager struct {
-		RegTokenDuration         time.Duration `json:"reg_token_duration"`
-		WorkerAuthTokenDuration  time.Duration `json:"worker_authtoken_duration"`
-		UserAccessTokenDuration  time.Duration `json:"user_accesstoken_duration"`
-		UserRefreshTokenDuration time.Duration `json:"user_refreshtoken_duration"`
-		TokenCleanupTicker       time.Duration `json:"token_cleanup_ticker"`
-		KeyCleanupTicker         time.Duration `json:"key_cleanup_ticker"`
-		InvalidTokenTTL          time.Duration `json:"invalid_token_ttl"`
-		SigningKeyTTL            time.Duration `json:"signing_key_ttl"`
-	}
-
 	// Vela is the extra set of Vela data passed to the compiler.
 	Vela struct {
 		Address              string        `json:"address"`
@@ -50,10 +38,9 @@ type (
 	// Metadata is the extra set of data passed to the compiler for
 	// converting a yaml configuration to an executable pipeline.
 	Metadata struct {
-		Database     *Database     `json:"database"`
-		Queue        *Queue        `json:"queue"`
-		Source       *Source       `json:"source"`
-		Vela         *Vela         `json:"vela"`
-		TokenManager *TokenManager `json:"token_manager"`
+		Database *Database `json:"database"`
+		Queue    *Queue    `json:"queue"`
+		Source   *Source   `json:"source"`
+		Vela     *Vela     `json:"vela"`
 	}
 )
